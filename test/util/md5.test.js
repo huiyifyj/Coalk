89a8ff4947b94e5f340ec9dcc86d3592caa68842
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-07 07:00:26 +0800

    Turn string to MD5 hash

---
+++ test/util/md5.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+const md5 = require('../../src/util/md5');
+
+// Instance Object for Testing
+const me = {
+    name: 'huiyifyj',
+    email: 'jxfengyijie@gmail.com',
+    url: 'http://huiyifyj.cn'
+}
+
+// Related MD5 Hash Value
+const md5Hash = {
+    name: 'f851f84309625ba941a1c99565039474',
+    email: 'c18c2dc71e66820e1d31c2ae1eec5d51',
+    url: '5cea194c52a2a7ebff46b2443727bcbd'
+}
+
+module.exports = describe('MD5 Test', function () {
+
+    it('"' + me.name + '" Converted to MD5 Value Successfully.', function () {
+        assert.equal(md5(me.name), md5Hash.name);
+    });
+
+    it('"' + me.email + '" Converted to MD5 Value Successfully.', function () {
+        assert.equal(md5(me.email), md5Hash.email);
+    });
+
+    it('"' + me.url + '" Converted to MD5 Value Successfully.', function () {
+        assert.equal(md5(me.url), md5Hash.url);
+    });
+
+});
