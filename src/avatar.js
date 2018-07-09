bd8d94d885982ff4780426de06cf0a3cf5089c3c
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-09 11:14:12 +0800

    Basically complete avatar

---
+++ src/avatar.js
@@ -0,0 +1,39 @@
+import md5 from './util/md5';
+
+/**
+ * Gravatar CDN
+ * https://gravatar.loli.net/avatar/~<MD5-value>
+ * You also can use https://gravatar.cat.net/avatar/~<MD5-value>
+ * or use https://cdn.v2ex.com/gravatar/~<MD5-value>
+ */
+const cdn = 'https://gravatar.loli.net/avatar/';
+
+let email = 'jxfengyijie@gmail.com';
+
+/**
+ * Use MD5 Module
+ * More API to https://github.com/blueimp/JavaScript-MD5#api
+ */
+let md5hash = md5(email);
+
+// function avatar () {
+//     return cdn + md5hash;
+// }
+
+class avatar {
+
+    constructor () {
+        this.email = email;
+
+        getAvatarUrl();
+    }
+
+    getAvatarUrl () {
+        let md5hash = md5(email);
+
+        return cdn + md5hash;
+    }
+
+}
+
+export default avatar;
