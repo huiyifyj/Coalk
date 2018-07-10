120537473898204e4a3dee28506364f9229f6885
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-10 12:04:16 +0800

    Remove annotation and add .gitattributes file

--- src/util/time.js
+++ src/util/time.js
@@ -28,24 +28,28 @@ class Time {
 
         var days = Math.floor(INTERVAL_TIME / (24 * 3600 * 1000));
         if (days === 0) {
-            //计算相差小时数
-            var leave1 = INTERVAL_TIME % (24 * 3600 * 1000); //计算天数后剩余的毫秒数
+
+            var leave1 = INTERVAL_TIME % (24 * 3600 * 1000);
             var hours = Math.floor(leave1 / (3600 * 1000));
+
             if (hours === 0) {
-                //计算相差分钟数
-                var leave2 = leave1 % (3600 * 1000); //计算小时数后剩余的毫秒数
+
+                var leave2 = leave1 % (3600 * 1000);
                 var minutes = Math.floor(leave2 / (60 * 1000));
+
                 if (minutes === 0) {
-                    //计算相差秒数
-                    var leave3 = leave2 % (60 * 1000); //计算分钟数后剩余的毫秒数
+
+                    var leave3 = leave2 % (60 * 1000);
                     var seconds = Math.round(leave3 / 1000);
+
                     return seconds + ' 秒前';
                 }
                 return minutes + ' 分钟前';
             }
             return hours + ' 小时前';
         }
-        if (days < 0) return '刚刚';
+        
+        if (days < 0) { return '刚刚' };
 
         if (days < 8) {
             return days + ' 天前';
