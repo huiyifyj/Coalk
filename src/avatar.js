98299b7eb3f5da1d9a6f018d6233faa363bb5087
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-11 15:38:02 +0800

    Update avatar module, it still have some problem

--- src/avatar.js
+++ src/avatar.js
@@ -1,37 +1,36 @@
 import md5 from './util/md5';
 
-/**
- * Gravatar CDN
- * https://gravatar.loli.net/avatar/~<MD5-value>
- * You also can use https://gravatar.cat.net/avatar/~<MD5-value>
- * or use https://cdn.v2ex.com/gravatar/~<MD5-value>
- */
-const cdn = 'https://gravatar.loli.net/avatar/';
-
-let email = 'jxfengyijie@gmail.com';
-
-/**
- * Use MD5 Module
- * More API to https://github.com/blueimp/JavaScript-MD5#api
- */
-let md5hash = md5(email);
-
-// function avatar () {
-//     return cdn + md5hash;
-// }
-
 class avatar {
 
-    constructor () {
+    constructor (cdn, email) {
+
+        /**
+         * Gravatar CDN
+         * https://gravatar.loli.net/avatar/~<MD5-value>
+         * You also can use https://gravatar.cat.net/avatar/~<MD5-value>
+         * or use https://cdn.v2ex.com/gravatar/~<MD5-value>
+         */
+        this.cdn = cdn;
         this.email = email;
 
         this.getAvatarUrl();
     }
 
     getAvatarUrl () {
+
+        const cdn = 'https://gravatar.loli.net/avatar/';
+
+        let email = 'jxfengyijie@gmail.com';
+
+        /**
+         * Use MD5 Module
+         * More API to https://github.com/blueimp/JavaScript-MD5#api
+         */
         let md5hash = md5(email);
 
         return cdn + md5hash;
+
+        console.log(cdn + md5hash)
     }
 
 }
