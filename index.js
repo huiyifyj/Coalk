533a9fc00b0e3a0c814547a3a9a1aeaff807d829
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-13 16:44:28 +0800

    Remove 'projectId' proprety configuration

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import app from './src/app';
 new app({
     // element: '',
     apiKey: "AIzaSyCacfaap-c6whB0mQiYoxzYN_1rchuZKJU",
-    databaseURL: "https://huiyi-fyj.firebaseio.com",
-    projectId: "huiyi-fyj"
+    databaseURL: "https://huiyi-fyj.firebaseio.com"
 });
