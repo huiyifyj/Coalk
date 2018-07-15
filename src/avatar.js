68c0b60b85a0d5a76da00b921633735d0082d0e6
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-15 23:54:10 +0800

    Update avatar param and function

--- src/avatar.js
+++ src/avatar.js
@@ -2,35 +2,41 @@ import md5 from './util/md5';
 
 class avatar {
 
+    /**
+     * About Gravatar CDN
+     * https://gravatar.loli.net/avatar/(MD5-value)
+     * You also can use https://gravatar.cat.net/avatar/(MD5-value)
+     * or use https://cdn.v2ex.com/gravatar/(MD5-value)
+     * 
+     * @param {string} cdn Gravatar CDN
+     * @param {string} email User email
+     * @constructor
+     */
     constructor (cdn, email) {
 
-        /**
-         * Gravatar CDN
-         * https://gravatar.loli.net/avatar/~<MD5-value>
-         * You also can use https://gravatar.cat.net/avatar/~<MD5-value>
-         * or use https://cdn.v2ex.com/gravatar/~<MD5-value>
-         */
-        this.cdn = cdn;
-        this.email = email;
+        this.getAvatarUrl(cdn, email);
 
-        this.getAvatarUrl();
     }
 
-    getAvatarUrl () {
-
-        const cdn = 'https://gravatar.loli.net/avatar/';
-
-        let email = 'jxfengyijie@gmail.com';
+    /**
+     * Get your gravatar image url.
+     *
+     * @param {string} cdn Gravatar CDN
+     * @param {string} email User email
+     * @return {string} Gravatar image url.
+     */
+    getAvatarUrl (cdn, email) {
 
         /**
          * Use MD5 Module
-         * More API to https://github.com/blueimp/JavaScript-MD5#api
+         * More API to [JavaScript-MD5](https://github.com/blueimp/JavaScript-MD5#api)
+         * 
+         * @type {string}
          */
         let md5hash = md5(email);
 
         return cdn + md5hash;
 
-        console.log(cdn + md5hash)
     }
 
 }
