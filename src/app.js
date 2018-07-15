db2c88297e5dc0fa0f631367390301c127ed9f65
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-15 13:35:23 +0800

    Finish footer nav stylesheet basically

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import handleOption from './util/option';
 
 import {input, footer, comment, noComment} from './view/view';
 
+import './scss/demo.scss';
+import './scss/index.scss';
+
+/**
+ * @class
+ */
 class App {
 
     /**
@@ -24,17 +30,16 @@ class App {
     init () {
 
         const ROOT_ELEMENT = this.option.container;
+
         const LANGUAGE = this.option.language;
 
         ROOT_ELEMENT.innerHTML += input;
 
         ROOT_ELEMENT.innerHTML += comment;
-        
+
         ROOT_ELEMENT.innerHTML += noComment;
 
         ROOT_ELEMENT.innerHTML += footer;
-
-        console.log(ROOT_ELEMENT)
 
     }
 
