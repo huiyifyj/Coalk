3d232adbc2fe046be816250075132706f170af4c
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-19 17:11:18 +0800

    Print log informaton about version and others in console

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ new app({
     apiKey: "AIzaSyCacfaap-c6whB0mQiYoxzYN_1rchuZKJU",
     databaseURL: "https://huiyi-fyj.firebaseio.com"
 });
+
+console.log(`\n
+FYJ | http://huiyifyj.cn | %c VERSION: ${'v0.0.1'}`, 'color: #000; background: #d7e5f5;');
