6290670d20075fcb47ce2b6454d44cb2a7cb6313
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-22 21:51:26 +0800

    Get version information by package.json file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import app from './src/app';
 
+import pkg from './package.json';
+
 new app({
     // element: '',
     apiKey: "AIzaSyCacfaap-c6whB0mQiYoxzYN_1rchuZKJU",
     databaseURL: "https://huiyi-fyj.firebaseio.com"
 });
 
+const VERSION = 'v' + pkg.version;
+
 // Print information on the console
-console.info(`https://github.com/huiyifyj/comment.js\n%c FYJ VERSION: ${'v0.0.1'}`, 'color: #cae5c5; background: #ff8b94;');
+console.info(`https://github.com/huiyifyj/comment.js\n%c FYJ VERSION: ${VERSION}`, 'color: #cae5c5; background: #ff8b94;');
