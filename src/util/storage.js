d97da9f4cf953d4d4f41d6ae8d6b478ee571de95
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-24 15:52:48 +0800

    Create storage.js for operating localStorage data

---
+++ src/util/storage.js
@@ -0,0 +1,41 @@
+/**
+ * Store some data by `localStorage`.
+ */
+const storage = window.localStorage;
+
+export default {
+
+    /**
+     * Store data into localStorage.
+     *
+     * @param {string} key The key of data that will be stored.
+     * @param {object} data The data object.
+     */
+    set: (key, data) => {
+        storage.setItem(key, JSON.stringify(data));
+    },
+
+    /**
+     * Get data from localStorage.
+     *
+     * @param {string} key The key of data stored.
+     * @return A JSON object that is converted from string.
+     */
+    get: (key) => {
+
+        const getString = storage.getItem(key);
+
+        return JSON.parse(getString);
+
+    },
+
+    /**
+     * Remove data stored by key.
+     *
+     * @param {string} key The key of data stored.
+     */
+    remove: (key) => {
+        storage.removeItem(key);
+    }
+
+}
