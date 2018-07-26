287808b2fc4158d834843f43bad8af34fb230e0f
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-26 19:30:48 +0800

    Update param: url(string) => data(object)

--- src/events/gravatarAPI.js
+++ src/events/gravatarAPI.js
@@ -1,21 +1,23 @@
 /**
  * Accept Gravatar JSON data by XMLHttpRequest.
+ * And verify its Authenticity.
  *
- * @param {string} url The JSON data url.
- * @return {Promise} Promise.
+ * @param {object} data The JSON data object, including name, emailhash property.
+ * @return {Promise} Promise: resolve a boolean value.
  */
-export default (url) => {
+export default (data) => {
 
     const promise = new Promise((resolve, reject) => {
 
         const xhr = new XMLHttpRequest();
 
-        xhr.open('GET', url, true);
+        xhr.open('GET', data.emailHash, true);
 
         xhr.onload = function () {
 
             if (this.status === 200) {
-                resolve(this.response);
+                const boolean = verify(this.response, data);
+                resolve(boolean);
             }
             else {
                 reject(new Error(this.statusText));
@@ -30,5 +32,26 @@ export default (url) => {
     });
 
     return promise;
+
+}
+
+/**
+ * Verify the data with gravatar JSON data.
+ *
+ * @param {object} response Get JSON object by XMLHttpRequest.
+ * @param {object} data Data to be verified. Note: the data include name and emailHash two property.
+ * @return {boolean}
+ */
+const verify = (response, data) => {
+
+    const name = response.entry[0].displayName;
+    const emailHash = response.entry[0].hash;
+
+    if (name == data.name && emailHash == data.emailHash) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
