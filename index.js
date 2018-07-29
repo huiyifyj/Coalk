44d2cf31507f6a6d5c10903fd5ef3e0fa898609d
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-29 18:06:06 +0800

    Remove my firebase configuration

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ import app from './src/app';
 
 import pkg from './package.json';
 
-new app({
-    apiKey: "AIzaSyCacfaap-c6whB0mQiYoxzYN_1rchuZKJU",
-    databaseURL: "https://huiyi-fyj.firebaseio.com"
-});
 
 const VERSION = 'v' + pkg.version;
 
 // Print information on the console
-console.info(`https://github.com/huiyifyj/comment.js\n%c FYJ VERSION: ${VERSION}`, 'color: #cae5c5; background: #ff8b94;');
+console.info(`https://github.com/huiyifyj/comment.js\n%c FYJ VERSION: ${VERSION}`, 'color: #cae5c5; background: #ff8b94;');export default app;
