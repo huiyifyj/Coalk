6687fe2824265d10f06fad1d0c2781ecc188a3ca
HuiyiFYJ <jxfengyijie@gmail.com>
2018-08-21 21:53:44 +0800

    Update the information that are printed in console

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import './src/scss/index.scss';
  * Print information on the console.
  * 'APP_VERSION' is defined in webpack config file.
  */
-console.info(`https://github.com/huiyifyj/comment.js\n%c ${APP_NAME.toUpperCase()} VERSION: ${APP_VERSION}`, 'color: #cae5c5; background: #ff8b94;');
+console.info(`%c${APP_NAME.toUpperCase()}%c\nVersion:${APP_VERSION}\nHuiyi.FYJ (https://github.com/huiyifyj/comment.js)\nCopyright ©${new Date().getFullYear()}`,'font-size: 18px; color: #3b3e43;', 'font-size: 12px; color: rgba(0,0,0,0.38);');
 
 export default app;
