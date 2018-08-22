aad080701aefd843dd4f4ee08d2d33986ab810f4
HuiyiFYJ <jxfengyijie@gmail.com>
2018-08-23 01:18:49 +0800

    Remove reply icon and update console words

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ import './src/scss/index.scss';
  * Print information on the console.
  * 'APP_VERSION' is defined in webpack config file.
  */
-console.info(`%c${APP_NAME.toUpperCase()}%c\nVersion: ${APP_VERSION}\nCopyright © ${new Date().getFullYear()}\nHuiyi.FYJ (https://github.com/huiyifyj/comment.js)`,'font-size: 18px; color: #3b3e43;', 'font-size: 12px; color: rgba(0,0,0,0.38);');
+console.info(`%c${APP_NAME.toUpperCase()}%c
+Version: ${APP_VERSION}
+Copyright © ${new Date().getFullYear()}
+Huiyi.FYJ (https://github.com/huiyifyj/comment.js)`,
+'font-size: 18px; color: #3b3e43;',
+'font-size: 12px; color: rgba(0,0,0,0.38);');
 
 export default app;
